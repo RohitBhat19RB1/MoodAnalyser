5f62e4db38adb21d79fa35fedeea9be5ff98e287
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-11-05 22:02:16 +0530

    Modified: Writing the Test Body

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,8 +1,13 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
     public void testMoodAnalysis() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad message");
 
+        Assert.assertTrue(mood, CoreMatchers.is("SAD" ));
     }
 }
